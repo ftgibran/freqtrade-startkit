76ffc4ccea3a01c1f8667201a96ce27fa6ee28e2
Felipe Gibran Eleuterio Toledo <ft.gibran@gmail.com>
2021-01-28 15:21:12 -0300

    fix ec2-userdata

--- ec2-userdata.sh
+++ ec2-userdata.sh
@@ -5,24 +5,21 @@ source ~/.bash_profile
 
 cd /home/ec2-user/
 
-sudo yum update -y
-sudo amazon-linux-extras install docker -y
-
-# Install git
-sudo yum install git -y
+yum update -y
+amazon-linux-extras install docker -y
 
 # Install JQ
-sudo yum install jq -y
+yum install jq -y
 
 # Install docker
-sudo yum install docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
+yum install docker
+service docker start
+usermod -a -G docker ec2-user
 
 # Install docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | tee /usr/local/bin/docker-compose > /dev/null
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 get_instance_tags () {
     INSTANCE_ID=$(/usr/bin/curl --silent http://169.254.169.254/latest/meta-data/instance-id)
@@ -51,11 +48,43 @@ tags_to_env "$instance_tags"
 mkdir ft_userdata
 cd ft_userdata/
 
+chown -R ec2-user ft_userdata/
+chmod -R 755 ft_userdata/
+
 # Download the docker-compose file from the repository
 curl https://raw.githubusercontent.com/freqtrade/freqtrade/stable/docker-compose.yml -o docker-compose.yml
+chown -R ec2-user docker-compose.yml
+chmod -R 755 docker-compose.yml
 
 # Pull the freqtrade image
 docker-compose pull
 
 # Create user directory structure
 docker-compose run --rm freqtrade create-userdir --userdir user_data
+
+cd user_data
+
+curl https://raw.githubusercontent.com/ftgibran/freqtrade-startkit/master/user_data/config.json -o config.json
+
+jq '.exchange.name = $newVal' --arg newVal ${EXCHANGE_NAME:-binance} config.json > tmp.$$.json && mv -f tmp.$$.json config.json
+jq '.exchange.key = $newVal' --arg newVal ${EXCHANGE_KEY:-''} config.json > tmp.$$.json && mv -f tmp.$$.json config.json
+jq '.exchange.secret = $newVal' --arg newVal ${EXCHANGE_SECRET:-''} config.json > tmp.$$.json && mv -f tmp.$$.json config.json
+
+jq '.telegram.token = $newVal' --arg newVal ${TELEGRAM_TOKEN:-''} config.json > tmp.$$.json && mv -f tmp.$$.json config.json
+jq '.telegram.chat_id = $newVal' --arg newVal ${TELEGRAM_CHAT_ID:-''} config.json > tmp.$$.json && mv -f tmp.$$.json config.json
+
+chown -R ec2-user config.json
+chmod -R 755 config.json
+
+cd strategies/
+
+curl https://raw.githubusercontent.com/ftgibran/freqtrade-startkit/master/user_data/strategies/standard.py -o standard.py
+
+chown -R ec2-user standard.py
+chmod -R 755 standard.py
+
+cd ../../
+
+docker-compose up -d
+
+docker-compose run freqtrade trade --dry-run -s Standard
